e4563927fb06809cdd9bf955f0feb295d81c1d51
ACI21 <72408392+ACI21@users.noreply.github.com>
2024-03-29 22:49:51 +0100

    .

--- Tarea 5/Ejercicio2.java
+++ Tarea 5/Ejercicio2.java
@@ -5,15 +5,15 @@ public class Ejercicio2 {
     public static void main(String[] args) {
         try {
             // Cargar el XML de entrada y el archivo XSLT
-            Source xmlSource = new StreamSource("artistas.xml");
-            Source xsltSource = new StreamSource("Ejercicio2.xslt");
+            Source xmlSource = new StreamSource("./Tarea 5/artistas.xml");
+            Source xsltSource = new StreamSource("./Tarea 5/Ejercicio2.xslt");
 
             // Preparar el resultado
             TransformerFactory factory = TransformerFactory.newInstance();
             Transformer transformer = factory.newTransformer(xsltSource);
 
             // Aplicar la transformación y escribir la salida en un archivo o imprimir en la consola
-            transformer.transform(xmlSource, new StreamResult("Ejercicio2.html"));
+            transformer.transform(xmlSource, new StreamResult("./Tarea 5/Ejercicio2.html"));
             System.out.println("Transformación completada. La salida se encuentra en 'Ejercicio2.html'.");
         } catch (Exception e) {
             e.printStackTrace();
